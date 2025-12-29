1179f983e7867986a1a79d1c347c7dc730f00349
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2025-12-29 10:26:50 -0300

    feat: adding supabase url and key

--- app/core/config.py
+++ app/core/config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 class Settings:
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
 
 settings = Settings()
